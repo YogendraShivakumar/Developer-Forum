a6083feb6d8f9e69269b6ebd3f501fe425c54745
Yogendra J S <yogs0616@netcracker.com>
2017-11-27 16:02:15 +0530

    reformat code

---
+++ src/main/java/com/netcracker/developerforum/DeveloperForumApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker.developerforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperForumApplication.class, args);
+	}
+}
